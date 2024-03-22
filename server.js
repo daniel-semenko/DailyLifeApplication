3bdb5aa686db444381862e57250806b5197889a2
Daniel Semenko <daniel.semenko@wsu.edu>
2024-03-21 21:01:38 -0700

    Successfully connected application to my MongoDB.

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 // not uploaded, yet it is still utilized by code. Requiring dotenv will load environment variables from env to process.env
 require('dotenv').config()
 
-import express from 'express';
-import { MongoClient } from 'mongodb';
+const express = require('express');
+const { MongoClient } =require('mongodb');
 
 
 // Creates an instance of the express server, which will be used to configure routes, middleware, and other settings. 
@@ -16,4 +16,26 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // mongo connection url
-const mongoURL = process.env.MongoURL;
+const mongoURL = process.env.MONGO_URL;
+
+console.log('mongoURL const: ', mongoURL);
+
+// Finally, we can initiate the connection to the MongoDB, utilizing the MongoClient.connect() function
+MongoClient.connect(mongoURL)
+    .then(client => {
+        console.log('Connected to MongoDB');
+        const db = client.db('DanielsPersonalAppCluster');
+
+        // routes and middleware go here
+
+        // start server
+        app.listen(PORT, () => {
+            // note: In JS, backticks ` ` are required for embedding expressions (in this case the string containing 
+            // the expression ${PORT}, must all be within backticks instead of single/double quotes)
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch(error =>
+        {
+            console.error('Error connecting to MongoDB: ', error)
+        });
